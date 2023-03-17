dbad6d750c32c22b830cfde9b0a556f1661ca3b5
asharma13524 <asharma13524@gmail.com>
2023-03-16 20:39:59 -0400

    Ownership ftw

---
+++ ownership/target/debug/deps/ownership-40f824acaa4eeab1.d
@@ -0,0 +1,5 @@
+/Users/asharma/rust-book/ownership/target/debug/deps/ownership-40f824acaa4eeab1: src/main.rs
+
+/Users/asharma/rust-book/ownership/target/debug/deps/ownership-40f824acaa4eeab1.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/Users/asharma/rust-book/ownership/target/debug/ownership: /Users/asharma/rust-book/ownership/src/main.rs
